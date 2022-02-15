6d7b6e4e811a1b65218b8bb255522974adc2a839
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-15 17:42:42 +0330

    fill data in admins model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $guarded = 'web';
 
     /**
      * The attributes that are mass assignable.
